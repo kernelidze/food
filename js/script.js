c60e0741084439da84e07f0e31c24f8be0e9d81f
kernelidze <kernelidze@gmail.com>
2022-09-21 03:29:56 +0300

    Добавил обработку таймера

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Tabs
+
 const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
@@ -34,3 +36,58 @@ tabsParent.addEventListener('click', (evt) => {
         });
     }
 });
+
+// Timer
+
+const deadline = '2022-12-25';
+
+function getTimeRemaining(endtime) {
+   const t = Date.parse(endtime) - Date.parse(new Date());
+   const days = Math.floor(t / (1000 * 60 * 60 * 24));
+   const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+   const minutes = Math.floor((t / 1000 / 60) % 60);
+   const seconds = Math.floor((t / 1000) % 60);
+
+   return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds 
+   };
+}
+
+function addZero(num) {
+    if (num >=0 && num < 10) {
+        return `0${num}`; 
+    } else {
+        return num;
+    }
+}
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = document.querySelector('#days');
+    const hours = document.querySelector('#hours');
+    const minutes = document.querySelector('#minutes');
+    const seconds = document.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); //чтобы не мигали часы при обновлении со старого значения, ведь строка выше ср. через секунду
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = addZero(t.days);
+        hours.innerHTML = addZero(t.hours);
+        minutes.innerHTML = addZero(t.minutes);
+        seconds.innerHTML = addZero(t.seconds);
+        
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('.timer', deadline);
+
